3526d2d3063fd77fdb470d23e2d2c4861e83b8d8
nisargdpatel <46580820+nisargdpatel@users.noreply.github.com>
2020-02-07 22:11:07 -0500

    Added firebase config in environment.ts

--- myPortfolio/src/environments/environment.ts
+++ myPortfolio/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyB6L_8QeL1ALeAb2_pd2HIWobfpDF-1zgs",
+    authDomain: "my-cs-portfolio.firebaseapp.com",
+    databaseURL: "https://my-cs-portfolio.firebaseio.com",
+    projectId: "my-cs-portfolio",
+    storageBucket: "my-cs-portfolio.appspot.com",
+    messagingSenderId: "898266001657",
+    appId: "1:898266001657:web:eb87a5b642b611549eb228",
+    measurementId: "G-Z43NVDDH5S"
+  }
 };
 
 /*
