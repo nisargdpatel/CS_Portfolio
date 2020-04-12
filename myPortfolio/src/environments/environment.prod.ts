c345bb388eab69b6d3b82872c0be3752d46369f2
nisargdpatel <46580820+nisargdpatel@users.noreply.github.com>
2020-04-11 21:39:44 -0400

    Added About Me section on the home page
    Took out background image on all pages

--- myPortfolio/src/environments/environment.prod.ts
+++ myPortfolio/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyB6L_8QeL1ALeAb2_pd2HIWobfpDF-1zgs",
+    authDomain: "my-cs-portfolio.firebaseapp.com",
+    databaseURL: "https://my-cs-portfolio.firebaseio.com",
+    projectId: "my-cs-portfolio",
+    storageBucket: "my-cs-portfolio.appspot.com",
+    messagingSenderId: "898266001657",
+    appId: "1:898266001657:web:eb87a5b642b611549eb228",
+    measurementId: "G-Z43NVDDH5S"
+  }
 };
